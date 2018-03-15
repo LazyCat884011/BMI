a94a876a121bdea58a378dac6e7ebf8aeeb8ee56
m305-65\w401 <tommy2007321@yahoo.com.tw>
2018-03-15 17:40:53 +0800

    :D

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,9 +10,21 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            double rate = 29.045;
-            Console.WriteLine("目前美金匯率是:" + rate);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("╔═══════════╗\n");
+            Console.Write("║                      ║\n");
+            Console.Write("║     D    M    A      ║\n");
+            Console.Write("║                      ║\n");
+            Console.Write("╚═══════════╝\n");
+            //double rate = 29.045;
+            //Console.WriteLine("目前美金匯率是:" + rate);
+            //Console.Write("請輸入台幣金額:");
+            //double input = double.Parse(Console.ReadLine());
+            //double end = input * rate;
+            //Console.Write(input+"台幣="+end+"美金");
             Console.ReadLine();
+            
             
         }
     }
